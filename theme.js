facf7f49a366385d21257700a857b79150b6c98d
Rostyslav Diachok <infernalmaster1@gmail.com>
2018-08-18 02:57:31 +0300

    first

---
+++ theme.js
@@ -0,0 +1,15 @@
+import theme from 'mdx-deck/themes'
+
+export default {
+  ...theme,
+  ol: {
+    fontSize: '1.2em'
+  },
+  img: {
+    maxWidth: '80%',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+  fontSizes: ['0.75em', '1em', '1.5em', '2em', '2.5em']
+}
