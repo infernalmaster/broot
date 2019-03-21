3d4c7135e6fbc142b5b3a6a89e46d5a54dd64ebe
Rostyslav Diachok <infernalmaster1@gmail.com>
2019-03-21 16:00:32 +0200

    add new presentation

--- theme.js
+++ theme.js
@@ -1,15 +1,36 @@
-import theme from 'mdx-deck/themes'
+import theme from "@mdx-deck/themes";
 
-export default {
-  ...theme,
-  ol: {
-    fontSize: '1.2em'
-  },
-  img: {
-    maxWidth: '80%',
-    display: 'block',
-    marginLeft: 'auto',
-    marginRight: 'auto'
-  },
-  fontSizes: ['0.75em', '1em', '1.5em', '2em', '2.5em']
-}
+import { syntaxHighlighter } from "mdx-deck/themes";
+
+const my = {
+  css: {
+    fontSize: "14px",
+    textAlign: "center",
+    "@media screen and (min-width:56em)": {
+      fontSize: "25px"
+    },
+    "@media screen and (min-width:64em)": {
+      fontSize: "40px"
+    },
+    "@media print": {
+      fontSize: "40px"
+    },
+    "li > p": {
+      margin: 0
+    },
+    "[name=wrapper]": {
+      padding: "10vw"
+    },
+    img: {
+      maxWidth: "70vw"
+    },
+    ul: {
+      fontSize: "0.7em"
+    },
+    ol: {
+      fontSize: "0.7em"
+    }
+  }
+};
+
+export default { ...theme, ...syntaxHighlighter, ...my };
